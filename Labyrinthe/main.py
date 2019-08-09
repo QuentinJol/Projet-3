df14764f51c897d10c0e1fecdd929d7061c06142
Quentin JOLY <dohkoo001@gmail.com>
2019-08-09 12:43:28 +0200

    Mise à jour du projet

--- Labyrinthe/main.py
+++ Labyrinthe/main.py
@@ -1,39 +1,59 @@
 # -*- coding: utf8 -*-
 
+import re
 
 
-mon_parcours = []
-liste_temporaire = []
-liste_temporaire2 = []
-fichier_parcours = "parcours.csv"
-direction = ""
-
+# Fonction pour le lecture du fichier labyrinthe
 def read_file(file):
     with open(file, 'r') as fichier:
-        return(fichier.read().splitlines())
-
-liste_temporaire = (read_file(fichier_parcours))
-
-for line in liste_temporaire:
-        liste_temporaire2.append("".join(line))
-
-for line in liste_temporaire2:
-        mon_parcours.append(list(line.strip()))   
+        liste_temporaire = []
+        temp_list2 = []
+        liste_temporaire=(fichier.read().splitlines())
+        for line in liste_temporaire:
+                temp_list2.append(list(line.strip()))   
+        return temp_list2
 
 
-mon_parcours[1][1] = "x"
+# Fonction d'affichage du labyrinthe
+def affichage_parcours(mon_parcours):
+        for line in mon_parcours:
+                print(''.join(line))
 
-for line in mon_parcours:
-        print(''.join(map(str, line)))
+# Fonction de récupération de l'entrée utilisateur pour la direction du joueur avec vérification du caractere saisi
+def entree_utilisateur():
+        direction = input("Dans quelle direction souhaitez-vous aller ?(Z, Q, S ou D pour jouer)")
+        character_control = re.findall("(?i)z|q|s|d", direction)
+        if (character_control) :
+                return (direction)
+        else:
+                entree_utilisateur()
+
+# def deplacement_personnage():
+#         if direction == "d":
+#                return (x =+ 1)
+#         else:
+#                 pass
 
 
-if mon_parcours[1][1] != "x":
-        print(type(mon_parcours[1][1]))
 
-while mon_parcours[15][10] != "x0" :
-        direction = input("Dans quelle direction souhaitez-vous aller ?")
-        if direction = "z":
+def main():
+        mon_parcours = []
+        fichier_parcours = "parcours"
+        direction ="a"
+        x = 1
+        y = 1
+        mon_parcours = read_file(fichier_parcours)
+        mon_parcours[y][x] = "p"
+        affichage_parcours(mon_parcours)
+        direction = entree_utilisateur()
+        print(direction)
 
 
+main()
 
-print(direction)
+#while mon_parcours[15][10] != "p" :
+ #       direction = input("Dans quelle direction souhaitez-vous aller ?")
+  #      if direction = "z":
+   #             if 
+
+
