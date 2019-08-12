23e53de6244c789c78876e3e5a43ac1ca3f2adf7
Quentin JOLY <dohkoo001@gmail.com>
2019-08-12 19:04:47 +0200

    Evolution du programme labyrinthe

--- Labyrinthe/main.py
+++ Labyrinthe/main.py
@@ -3,55 +3,78 @@
 import re
 
 
-# Fonction pour le lecture du fichier labyrinthe
 def read_file(file):
-    with open(file, 'r') as fichier:
-        liste_temporaire = []
-        temp_list2 = []
-        liste_temporaire=(fichier.read().splitlines())
-        for line in liste_temporaire:
-                temp_list2.append(list(line.strip()))   
+        """
+        Reading function for Labyrinthe file
+        """
+        with open(file, 'r') as fichier:
+                temp_list = []
+                temp_list2 = []
+                temp_list=(fichier.read().splitlines())
+                for line in temp_list:
+                        temp_list2.append(list(line.strip()))   
         return temp_list2
 
 
-# Fonction d'affichage du labyrinthe
-def affichage_parcours(mon_parcours):
-        for line in mon_parcours:
+
+def lab_display(course):
+        """
+        Display labyrinthe function
+        """
+        for line in course:
                 print(''.join(line))
 
-# Fonction de récupération de l'entrée utilisateur pour la direction du joueur avec vérification du caractere saisi
-def entree_utilisateur():
+def user_input():
+        """
+        User input function for direction
+        """
         direction = input("Dans quelle direction souhaitez-vous aller ?(Z, Q, S ou D pour jouer)")
         character_control = re.findall("(?i)z|q|s|d", direction)
         if (character_control) :
+                print(direction)
                 return (direction)
         else:
-                entree_utilisateur()
+                user_input()
 
-# def deplacement_personnage():
-#         if direction == "d":
-#                return (x =+ 1)
-#         else:
-#                 pass
-
+def player_movement(x, y, direction):
+        if direction == "d":
+                x += 1
+                print(x)
+                return(x, y)
+        elif direction == "z":
+                y -= 1
+                return (x, y)
+        elif direction == "q":
+                x -= 1
+                return(x, y)
+        elif direction == "s":
+                y += 1
+                return(x, y)
+        else:
+                pass
 
 
 def main():
-        mon_parcours = []
-        fichier_parcours = "parcours"
+        course = []
+        course_file = "parcours"
         direction ="a"
         x = 1
         y = 1
-        mon_parcours = read_file(fichier_parcours)
-        mon_parcours[y][x] = "p"
-        affichage_parcours(mon_parcours)
-        direction = entree_utilisateur()
+        course = read_file(course_file)
+        course[y][x] = "p"
+        lab_display(course)
+        direction = user_input()
+        course[y][x] = " "
+        x, y = (player_movement(x,y, direction))
+        print(x, y)
+        course[y][x] = "x"
         print(direction)
+        lab_display(course)
 
 
 main()
 
-#while mon_parcours[15][10] != "p" :
+#while course[15][10] != "p" :
  #       direction = input("Dans quelle direction souhaitez-vous aller ?")
   #      if direction = "z":
    #             if 
