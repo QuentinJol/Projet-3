16a48524c55ca9837a7381e838bd97091a1dd095
Quentin JOLY <dohkoo001@gmail.com>
2019-10-18 08:56:01 +0200

    Ajout des ressources graphiques et modification du code pour l'intégration de PyGame.

--- Labyrinthe/main_POO.py
+++ Labyrinthe/main_POO.py
@@ -6,13 +6,24 @@ import os
 import pygame
 from pygame.locals import *
 
+pygame.init()
+
+window = pygame.display.set_mode((600, 480), RESIZABLE)
+floor = pygame.image.load("ressources/floor.png").convert()
+wall = pygame.image.load("ressources/wall.png").convert()
+guard = pygame.image.load("ressources/guard.png").convert()
+gamer = pygame.image.load("ressources/MacGyver20px.png").convert()
+object1 = pygame.image.load("ressources/ether20.png").convert()
+object2 = pygame.image.load("ressources/seringue20.png").convert()
+object3 = pygame.image.load("ressources/aiguille20.png").convert()
+# window.blit(floor, (0, 0))                
 
 class Map :
 
         def __init__(self, file):
 
                 self.file = file
-
+                self.course = []
 
         def read_file(self):
                 """
@@ -26,60 +37,82 @@ class Map :
                                 temp_list=(fichier.read().splitlines())
                                 for line in temp_list:
                                         temp_list2.append(list(line.strip()))   
-                        return temp_list2
+                        self.course = temp_list2
                 except IOError:
                         print("Erreur! Le fichier \"parcours\" n'a pas pu être ouvert.")
                         quit()
                 
-        def player_start_position(self, x, y, course):
-                course[y][x] = "p"
-                return(course)
+        def player_start_position(self, x, y):
+                self.course[y][x] = "p"
+
+        def lab_display_graphic(self, window):
+                y = 0
+                i = 1
+                while y < 17 :
+                        x = 0
+                        for value in self.course[y]:
+                                if value == "a" :
+                                        window.blit(object1, (x*20, y*20))
+                                if value == "b" :
+                                        window.blit(object2, (x*20, y*20))
+                                if value == "c" :
+                                        window.blit(object3, (x*20, y*20))
+                                if value == "G" :
+                                        window.blit(guard, (x*20, y*20))  
+                                if value == "p":
+                                        window.blit(gamer, (x*20, y*20))
+                                if value == "#" :
+                                        window.blit(wall, (x*20, y*20))
+                                if value == " " :
+                                        window.blit(floor, (x*20, y*20))           
+                                x += 1 
+                        y += 1
 
 
-
-        def lab_display(self, course : list):
+        def lab_display(self):
                 """
                 Display labyrinthe function
                 """
-                for line in course:
+                for line in self.course:
                         print(''.join(line))
 
 
-        def objects_position(self, course : list):
+
+        def objects_position(self):
                 """
                 Objects positioning in labyrinthe
                 """
                 i = 0
                 y = 1
-                x = 1   
+                x = 1 
+                objects = ["a", "b", "c"]  
                 while i < 3 :
                         y = randint(1, 15)
                         x = randint(1, 15)
-                        if course[y][x] == " " :
-                                course[y][x]= "o"
+                        if self.course[y][x] == " " :
+                                self.course[y][x]= objects[i]
                                 i += 1
-                return course
 
 
-        def guardian_control(self, x, y, course, objects, guardian):
-                if course[y][x+1] == "G" or course[y][x-1] == "G" :
-                                if objects == 3:
-                                        guardian = 1
-                                        print("Vous avez gagné !!!")
-                                        return guardian
-                                else :
-                                        guardian = 1
-                                        print("Vous avez perdu !")
-                                        return guardian
+        def guardian_control(self, x, y, objects):
+                if self.course[y][x+1] == "G" or self.course[y][x-1] == "G" :
+                        if objects == 3:
+                                print("Vous avez gagné !!!")
+                                return True
+                        else :
+                                print("Vous avez perdu !")
+                                return True
 
 
 class Player:
 
-        def __init__(self, x, y, course, direction, objects):
+        def __init__(self, x, y, lab, objects):
 
                 self.x = x
                 self.y = y
                 self.objects = objects
+                self.lab = lab
+                
 
         def user_input(self):
                 """
@@ -90,21 +123,30 @@ class Player:
                 character_control = re.search("z|q|s|d", direction)
                 if (character_control) :
                         return (direction)
-                Player.user_input(self)
+                self.user_input()
 
-        def player_movement(self, course, direction):
-                if direction == "d" and course[self.y][self.x + 1] in [" ", "o"]: 
-                        self.x += 1    
-                elif direction == "z" and course[self.y - 1][self.x] in [" ", "o"]:
+        def player_movement(self):
+                direction = self.user_input()
+                if direction == "d" and self.lab.course[self.y][self.x + 1] in [" ", "o"]: 
+                        self.lab.course[self.y][self.x] = " "
+                        self.x += 1
+                        self.lab.course[self.y][self.x] = "p" 
+                elif direction == "z" and self.lab.course[self.y - 1][self.x] in [" ", "o"]:
+                        self.lab.course[self.y][self.x] = " "
                         self.y -= 1
-                elif direction == "q" and course[self.y][self.x - 1] in [" ", "o"]:
+                        self.lab.course[self.y][self.x] = "p" 
+                elif direction == "q" and self.lab.course[self.y][self.x - 1] in [" ", "o"]:
+                        self.lab.course[self.y][self.x] = " "
                         self.x -= 1
-                elif direction == "s" and course[self.y + 1][self.x] in [" ", "o"]:
+                        self.lab.course[self.y][self.x] = "p" 
+                elif direction == "s" and self.lab.course[self.y + 1][self.x] in [" ", "o"]:
+                        self.lab.course[self.y][self.x] = " "
                         self.y += 1
+                        self.lab.course[self.y][self.x] = "p" 
                 return(self.x, self.y)
 
-        def objects_capture(self, course):
-                if course[self.y][self.x] == "o":
+        def objects_capture(self):
+                if self.lab.course[self.y][self.x] == "o":
                         self.objects += 1
                         print("Vous avez acquis un objet")
                         return self.objects
@@ -114,26 +156,24 @@ class Player:
                                 
 
 def main():
-        course = Map("parcours")
-        direction ="a"
-        guardian = 0
-        objects = 0
-        x = 1
-        y = 1
-        lab = course.read_file
-        lab = course.player_start_position(x, y, course)
-        lab = course.objects_position(course)
-        print (lab)
-        # while guardian != 1 :
-        #         print("\n")
-        #         direction = user_input()
-        #         course[y][x] = " "
-        #         x, y = (player_movement(x,y, course, direction))
-        #         objects = objects_capture(x, y, course, objects)
-        #         course[y][x] = "p"
-        #         os.system("clear")
-        #         lab_display(course)
-        #         guardian = guardian_control(x, y, course, objects, guardian)
+        labyrinthe = Map("parcours")
+        guardian = False
+        labyrinthe.read_file()
+        player1 = Player(1, 1, labyrinthe, 0)
+        labyrinthe.player_start_position(player1.x, player1.y)
+        labyrinthe.objects_position()
+        labyrinthe.lab_display()
+        labyrinthe.lab_display_graphic(window)
+        pygame.display.flip()
+        while not guardian :
+                print("\n")
+                player1.player_movement()
+                player1.objects_capture()
+                os.system("clear")
+                labyrinthe.lab_display()
+                labyrinthe.lab_display_graphic(window)
+                pygame.display.flip()
+                guardian = labyrinthe.guardian_control(player1.x, player1.y, player1.objects)
 
 
 
